b9413c0f70e3f1d9993149a8f9f3af3f84fbc078
george <georgel@appturn.com>
2014-03-31 22:13:47 +0300

    InputManager added

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+	public enum EInputState
+	{
+		Normal,
+		MouseDown,
+	}
+
+	private EInputState inputState;
+
+	private static InputManager instance = null;
+	public static InputManager Instance
+	{ get { return instance; } }
+
+	public System.Action<Vector2> OnClick = null;
+	void Awake()
+	{
+		if (null != instance)
+		{
+			Debug.LogError("There are more than one InputManager on the scene");
+		}
+		instance = this;
+
+		inputState = EInputState.Normal;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && inputState != EInputState.MouseDown)
+		{
+			inputState = EInputState.MouseDown;
+			if (null != OnClick)
+				OnClick(Input.mousePosition);
+
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			inputState = EInputState.Normal;
+		}
+	}
+
+	void OnDestroy()
+	{
+		if (instance == this)
+		{
+			instance = null;
+		}
+		OnClick = null;
+	}
+}
