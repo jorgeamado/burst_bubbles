6065281daa79f1f673ead72f8b3619be7353e5f0
george <georgel@appturn.com>
2014-03-31 22:19:50 +0300

    Helper class

---
+++ Assets/Classes/Helpers/Helper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+#if UNITY_4_3
+using UnityEngine;
+#endif
+
+public class Helper
+{
+	public static TEnum StringToEnum<TEnum>(string strEnumValue, TEnum defaultValue)
+	{
+		var str = strEnumValue.Trim();
+		if (Enum.IsDefined(typeof(TEnum), str))
+			return (TEnum)Enum.Parse(typeof(TEnum), str);
+		else
+			return defaultValue;
+	}
+}
+
+public class Logger
+{
+	public static void Log(string message)
+	{
+		#if UNITY_4_3
+		Debug.Log(message);
+		#else
+		Console.WriteLine(message);
+		#endif
+	}
+
+	public static void LogWarning(string message)
+	{
+		#if UNITY_4_3
+		Debug.LogWarning(message);
+		#else
+		Console.WriteLine("<WARNING> " + message);
+		#endif
+	}
+	public static void LogError(string message)
+	{
+		#if UNITY_4_3
+		Debug.LogError(message);
+		#else
+		Console.WriteLine("<ERROR> " + message);
+		#endif
+	}
+}
