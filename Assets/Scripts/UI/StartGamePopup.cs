ba35447a0af67234d4436e154fc5aea69f782349
george <georgel@appturn.com>
2014-03-31 22:21:43 +0300

    UI

---
+++ Assets/Scripts/UI/StartGamePopup.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGamePopup : MonoBehaviour
+{
+	private Network network;
+	void Start ()
+	{
+		network = FindObjectOfType<Network>();
+	}
+
+	string ip = "127.0.0.1";
+	int port = 14001;
+
+	void OnGUI()
+	{
+		if(network.UIStatus == Network.EUIStatus.WaitingForConnection)
+		{
+			GUI.Box(new Rect(10,10,250,90), "");
+			GUI.Label(new Rect(20, 20, 60, 20), network.PlayerName);
+
+			GUI.Label(new Rect(20, 40, 60, 20), network.OtherPlayerName);
+			GUI.Label(new Rect(100, 40, 100, 20), network.OpponentInfo);
+
+			if (GUI.Button(new Rect(20, 60, 100, 20), "Start Game"))
+			{
+				network.Ready();
+			}
+
+			if (!network.IsOtherPlayerConnected)
+			{
+				GUI.Label(new Rect(320, 60, 80, 20), "Address");
+				ip = GUI.TextField(new Rect(380, 60, 80, 20), ip);
+				port = int.Parse(GUI.TextField(new Rect(460, 60, 50, 20), port.ToString()));
+
+				if (GUI.Button(new Rect(320, 60, 60, 20), "Connect"))
+				{
+					network.ConnectTo(ip, port);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI/StartPopup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPopup : MonoBehaviour
+{
+	Network network;
+	LevelManager levelmanager;
+
+	void Start()
+	{
+		network = FindObjectOfType<Network>();
+	}
+
+	void OnGUI()
+	{
+		if (network.UIStatus == Network.EUIStatus.EnteringName)
+		{
+			GUI.Box(new Rect(10, 10, 250, 120), "");
+			GUI.Label(new Rect(20, 40, 80, 20), "Name");
+			network.PlayerName = GUI.TextField(new Rect(80, 40, 80, 20), network.PlayerName);
+
+			GUI.Label(new Rect(20, 60, 80, 20), "Address");
+			network.ip = GUI.TextField(new Rect(80, 60, 80, 20), network.ip);
+			network.port = int.Parse(GUI.TextField(new Rect(160, 60, 50, 20), network.port.ToString()));
+
+			if (GUI.Button(new Rect(20, 100, 80, 20), "StartGame"))
+			{
+				network.StartListening();
+			}
+		}
+	}
+}
